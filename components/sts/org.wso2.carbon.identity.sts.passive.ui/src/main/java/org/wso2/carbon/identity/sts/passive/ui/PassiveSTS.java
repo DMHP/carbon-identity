0e676d7c2a53ffc2c035e0ed61f0ee2731bc71a1
hasanthiP <hasanthidisanayakka@yahoo.com>
2015-10-28 17:37:10 +0530

    Formatting issues

--- components/sts/org.wso2.carbon.identity.sts.passive.ui/src/main/java/org/wso2/carbon/identity/sts/passive/ui/PassiveSTS.java
+++ components/sts/org.wso2.carbon.identity.sts.passive.ui/src/main/java/org/wso2/carbon/identity/sts/passive/ui/PassiveSTS.java
@@ -288,8 +288,6 @@ public class PassiveSTS extends HttpServlet {
                                                String sessionDataKey, SessionDTO sessionDTO) throws IOException {
 
         String commonAuthURL = IdentityUtil.getServerURL(FrameworkConstants.COMMONAUTH, true);
-
-
         String selfPath = request.getRequestURI();
         //Authentication context keeps data which should be sent to commonAuth endpoint
         AuthenticationRequest authenticationRequest = new AuthenticationRequest();
