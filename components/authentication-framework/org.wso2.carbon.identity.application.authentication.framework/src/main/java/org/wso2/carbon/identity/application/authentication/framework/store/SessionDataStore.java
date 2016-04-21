e5dd6079814174126a18c91196223d6bd21d1cb0
Johann Dilantha Nallathamby <johann@Johanns-MacBook-Pro.local>
2016-04-22 03:25:16 +0530

    Fixing IDENTITY-4516

--- components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/store/SessionDataStore.java
+++ components/authentication-framework/org.wso2.carbon.identity.application.authentication.framework/src/main/java/org/wso2/carbon/identity/application/authentication/framework/store/SessionDataStore.java
@@ -270,9 +270,10 @@ public class SessionDataStore {
             resultSet = preparedStatement.executeQuery();
             if(resultSet.next()) {
                 String operation = resultSet.getString(1);
+                long timestamp = resultSet.getLong(3)/1000000;
                 if ((OPERATION_STORE.equals(operation))) {
-                    return new SessionContextDO(key, type, getBlobObject(resultSet.getBinaryStream(2)), new Timestamp
-                            (resultSet.getLong(3)));
+                    return new SessionContextDO(key, type, getBlobObject(resultSet.getBinaryStream(2)),
+                            new Timestamp(timestamp));
                 }
             }
         } catch (ClassNotFoundException | IOException | SQLException |
@@ -324,7 +325,14 @@ public class SessionDataStore {
         }
         try {
             statement = connection.prepareStatement(sqlDeleteExpiredDataTask);
-            statement.setLong(1, timestamp.getTime()*1000000);
+
+            // create a nano time stamp relative to Unix Epoch
+            long currentStandardNano = timestamp.getTime() * 1000000;
+            long currentSystemNano = System.nanoTime();
+            currentStandardNano = currentStandardNano + (currentSystemNano - FrameworkServiceDataHolder.getInstance()
+                    .getNanoTimeReference());
+
+            statement.setLong(1, currentStandardNano);
             statement.execute();
             if (!connection.getAutoCommit()) {
                 connection.commit();
@@ -469,7 +477,14 @@ public class SessionDataStore {
                 }
             }
             statement = connection.prepareStatement(sqlDeleteSTORETask);
-            statement.setLong(1, timestamp.getTime());
+
+            // create a nano time stamp relative to Unix Epoch
+            long currentStandardNano = timestamp.getTime() * 1000000;
+            long currentSystemNano = System.nanoTime();
+            currentStandardNano = currentStandardNano + (currentSystemNano - FrameworkServiceDataHolder.getInstance()
+                    .getNanoTimeReference());
+
+            statement.setLong(1, currentStandardNano);
             statement.execute();
             if (!connection.getAutoCommit()) {
                 connection.commit();
@@ -495,7 +510,14 @@ public class SessionDataStore {
         }
         try {
             statement = connection.prepareStatement(sqlDeleteDELETETask);
-            statement.setLong(1, timestamp.getTime());
+
+            // create a nano time stamp relative to Unix Epoch
+            long currentStandardNano = timestamp.getTime() * 1000000;
+            long currentSystemNano = System.nanoTime();
+            currentStandardNano = currentStandardNano + (currentSystemNano - FrameworkServiceDataHolder.getInstance()
+                    .getNanoTimeReference());
+
+            statement.setLong(1, currentStandardNano);
             statement.execute();
             if (!connection.getAutoCommit()) {
                 connection.commit();
